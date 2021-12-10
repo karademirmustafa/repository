c7b3e690716ef67993db4fed822533493b06fcb4
karademirmustafa <karademirbayburt@gmail.com>
2021-12-10 15:58:36 +0300

    edited is navbar

--- website-design/client/src/App.js
+++ website-design/client/src/App.js
@@ -1,13 +1,13 @@
 
 import './app.css';
 import Navbar from './components/navbar/Navbar';
-import Navbar2 from './components/Navbar2';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="app">
    <Topbar/>
+   <Navbar/>
     </div>
   );
 }
--- website-design/client/src/components/navbar/Navbar.jsx
+++ website-design/client/src/components/navbar/Navbar.jsx
@@ -1,32 +1,44 @@
 import "./navbar.css";
-import { Facebook, Instagram, Phone, Twitter } from "@material-ui/icons";
+import { Search, Cancel } from "@material-ui/icons";
+import { useState } from "react";
 export default function Navbar() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleClick = () => {
+    setShowSearch(false);
+  };
+
   return (
     <div className="navbar">
       <div className="navbarLeft">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"
-          alt=""
-          className="navbarLogoImg"
-        />
-
-        <span className="navbarPhone">
-  
-          <Phone className="navbarIconPhone" /> +90 538 700 3428
-        </span>
-      </div>
-      <div className="navbarCenter">
-        <ul className="navbarList">
-          <div className="navbarListItem">Anasayfa</div>
-          <div className="navbarListItem">İletişim</div>
-          <div className="navbarListItem">Biz Kimiz?</div>
-          <div className="navbarListItem">Hemen Ara!</div>
-        </ul>
+        <div className="navbarLeftLogo">
+          <img
+            src="https://www.baglicakalbensaglikkabini.com/wp-content/uploads/2020/12/baglica-kalben-saglik-kabini-logo-2.png"
+            alt=""
+            className="navbarLogo"
+          />
+        </div>
       </div>
       <div className="navbarRight">
-        <Instagram className="navbarIcon" />
-        <Twitter className="navbarIcon" />
-        <Facebook className="navbarIcon" />
+        <ul className="navbarRightList">
+          <li className="navbarRightListItem">Anasayfa</li>
+          <li className="navbarRightListItem">Hakkımızda</li>
+          <li className="navbarRightListItem">ANKARA EVDE SAĞLIK</li>
+          <li className="navbarRightListItem">Hizmetlerimiz</li>
+          <li className="navbarRightListItem">Blog</li>
+          <li className="navbarRightListItem">İletişim</li>
+    <span className="navbarIcon">
+          <Search
+            onClick={() => {
+              setShowSearch(true);
+            }}
+            className={
+              showSearch
+                ? "navbarSearchIcon showSearchIcon"
+                : "navbarSearchIcon"
+            }
+          /></span>
+        </ul>
       </div>
     </div>
   );
